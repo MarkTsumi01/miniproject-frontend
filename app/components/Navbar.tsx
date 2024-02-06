e8011676b2fa4d98b86f07835e76ae27aec532f4
marktsumi <kanraveemark01@gmail.com>
2024-02-06 16:25:36 +0700

    create navbar & config nextui

--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -6,41 +6,30 @@ import {
   NavbarItem,
   Link,
   Button,
+  Input,
 } from "@nextui-org/react";
 // import { AcmeLogo } from "./AcmeLogo.jsx";
 
-export default function Navbars() {
+export default function Nav() {
   return (
-    <Navbar>
+    <Navbar position="sticky" className=" bg-white">
       <NavbarBrand>
         {/* <AcmeLogo /> */}
-        <p className="font-bold text-inherit">ACME</p>
+        <p className="font-bold text-inherit">Blackdit</p>
       </NavbarBrand>
       <NavbarContent className="hidden sm:flex gap-4" justify="center">
         <NavbarItem>
-          <Link color="foreground" href="#">
-            Features
-          </Link>
-        </NavbarItem>
-        <NavbarItem isActive>
-          <Link href="#" aria-current="page">
-            Customers
-          </Link>
-        </NavbarItem>
-        <NavbarItem>
-          <Link color="foreground" href="#">
-            Integrations
-          </Link>
+          <Input
+            type="email"
+            placeholder="Search Post"
+            color="primary"
+            isClearable
+          />
         </NavbarItem>
       </NavbarContent>
       <NavbarContent justify="end">
         <NavbarItem className="hidden lg:flex">
           <Link href="#">Login</Link>
-        </NavbarItem>
-        <NavbarItem>
-          <Button as={Link} color="primary" href="#" variant="flat">
-            Sign Up
-          </Button>
         </NavbarItem>
       </NavbarContent>
     </Navbar>
